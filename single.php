bc2db756ec9d83bf53ecc8e760bc3be965078586
ayumuohtaki <ayumu0821@gmail.com>
2022-03-28 22:10:19 +0900

    コピー

--- single.php
+++ single.php
@@ -14,8 +14,4 @@ endif;
                   </div>
                 </div>
               </div>
-              
-              <a href="https://blogmura.com/ranking/in?p_cid=11065693" target="_blank"><img src="https://b.blogmura.com/original/1246813"  alt="にほんブログ村 にほんブログ村へ" /></a>
-              <div class="uniik_logo"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/common/uniik_logo.png" alt="PARRAMOTA~30minutes~" /></div>
-              
 <?php get_footer(); ?>
